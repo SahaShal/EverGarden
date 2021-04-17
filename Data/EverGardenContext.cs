6b8e4484439a6c960faebad28fb5ccfaa9351fc4
SahaShal <sagyndykahiat@gmail.com>
2021-04-17 08:45:38 +0600

    first commit

---
+++ Data/EverGardenContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EverGarden.Models;
+
+namespace EverGarden.Data
+{
+    public class EverGardenContext : DbContext
+    {
+        public EverGardenContext(DbContextOptions<EverGardenContext> options) : base(options)
+        { }
+
+        public DbSet<Plant> Plant { get; set; } 
+    }
+}
---
+++ Models/Plant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace EverGarden.Models
+{
+    public class Plant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ScientificName { get; set; }
+        public string CategoryWhere { get; set; }
+        public string CategoryEdible { get; set; }
+        public string Info { get; set; }
+        public string Accecories { get; set; }
+    }
+}
